94d6f45b5aacf4cced0da53caefa7c3dd12b58a9
Unknown <e0031272@u.nus.edu>
2018-09-27 15:10:35 +0800

    Update to v1.8.0

--- mc_att_control.hpp
+++ mc_att_control.hpp
@@ -247,8 +247,6 @@ private:
 		(ParamFloat<px4::params::SENS_BOARD_Y_OFF>) _board_offset_y,
 		(ParamFloat<px4::params::SENS_BOARD_Z_OFF>) _board_offset_z,
 
-		(ParamFloat<px4::params::VT_WV_YAWR_SCL>) _vtol_wv_yaw_rate_scale,		/**< Scale value [0, 1] for yaw rate setpoint  */
-
 		(ParamBool<px4::params::MC_CNF_ENABLE>) _cnf_p_enabled,
 
 		(ParamFloat<px4::params::MC_CNF_F1>) _cnf_f1,
